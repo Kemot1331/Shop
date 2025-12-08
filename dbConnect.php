8870d38dfefe3a8cc92a0148603a821639464234
Kemot <tomekjarzebak123@gmail.com>
2025-12-08 21:41:28 +0100

    121312

--- dbConnect.php
+++ dbConnect.php
@@ -5,6 +5,7 @@
     $db = "Shop";
 
     $conn = mysqli_connect($adress, $user, $password, $db);
+    $conn->set_charset("utf8mb4");
 
     // if(!$conn){
     //     die("Connection problem".mysqli_connect_error());
--- index.php
+++ index.php
@@ -1,22 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MyShop</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <header>
-        <h2><a href="index.php">MyShop</a></h2>
-        <div class="cart">
-            <a href="cart.php">
-                <span>Koszyk</span>
-                <span>(1)</span>
-                <span>0 zł</span>
-            </a>
-        </div>
-    </header>
+<?php 
+    include "includes/header.php";
+?>
     <section class="mainContainer">
         <main>
             <?php 
